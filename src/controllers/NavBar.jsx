b0bd66bf0d533826fd9548b497703ca8988334be
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-08-10 19:22:02 -0400

    navbar setup

---
+++ src/controllers/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+   return( 
+        <nav>
+           <div><span>Q</span>uotes</div>
+           <p className="nav-btn">New Quote</p>
+        </nav>
+    )
+}
+
+export default NavBar;
