e42040c7ec3e0047d73d579dc71f18b075e5a5f2
Lakisha J <lakishajohnson@pursuit.org>
2023-08-10 19:40:02 -0400

    Index Page

---
+++ src/Pages/Index.js
@@ -0,0 +1,12 @@
+import Quotes from "../Components/Quotes";
+
+function Index() {
+  return (
+    <div className="Index">
+      <div><h2>Index</h2></div>
+      <Quotes />
+    </div>
+  );
+}
+
+export default Index;
