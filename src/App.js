11c1580644143e646c0490a7423ebe1007427848
Lakisha J <lakishajohnson@pursuit.org>
2023-08-10 19:38:43 -0400

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+
+import Footer from "./Components/Footer";
+
+import Index from "./Pages/Index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/quotes" element={<Index />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
