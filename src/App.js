a3d6e4abdc33c55a439f0144ac5f98e1f4763e7d
Lakisha J <lakishajohnson@pursuit.org>
2023-08-11 20:48:16 -0400

    Show page route path added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 
-import Footer from "./Components/Footer";
+import Footer from "./components/Footer";
 
 import Index from "./pages/Index";
-import NavBar from "./controllers/NavBar";
 import Home from "./pages/Home";
+
+import NavBar from "./controllers/NavBar";
+import Show from "./pages/Show";
 
 function App() {
   return (
     <div>
       <Router>
-      <NavBar />
-      {/* <Home /> */}
-          <Routes>
-            <Route path="/" element = {<Home />} />
-            <Route path="/quotes" element={<Index />} />
-          </Routes>
-          <Footer />
-        </Router>
-        
-      
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quotes" element={<Index />} />
+          <Route path="/quotes/:id" element={<Show />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
