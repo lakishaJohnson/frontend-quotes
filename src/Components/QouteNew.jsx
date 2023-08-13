632451a2ed358de01114f8e89355bc6e5625b0ab
Lakisha J <lakishajohnson@pursuit.org>
2023-08-13 14:31:19 -0400

    //eslint-disable-next-line

--- src/Components/QouteNew.jsx
+++ src/Components/QouteNew.jsx
@@ -1,126 +1,155 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import ErrorModal from "./ErrorModal";
 
-const API = process.env.REACT_APP_API_URL
+const API = process.env.REACT_APP_API_URL;
 
 function QuoteNew() {
-    const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-   
-    const addQuote = (newQuote) => {
-        axios
-        .post(`${API}/quotes`, newQuote)
-        .then(() => navigate('/quotes'))
-        .catch((e) => console.warn("catch", e))
-    } 
-    
-    const [quote, setQoute] = useState({
-        author:"",
-        quote:"",
-        category:"",
-        source:"",
-        year_quoted:"",
-        language:"",
-        is_favorite:false,
-    });
+  const navigate = useNavigate();
 
-    const handleCheckboxChange = () => {
-        setQoute({...quote, is_favorite: !quote.is_favorite});
+  const addQuote = (newQuote) => {
+    axios
+      .post(`${API}/quotes`, newQuote)
+      .then(() => navigate("/quotes"))
+      .catch((e) => {
+        setErrorMessage(e.message || "An error occurred.");
+        setShowModal(true);
+      });
+  };
+
+  const [quote, setQuote] = useState({
+    author: "",
+    quote: "",
+    category: "",
+    source: "",
+    year_quoted: "",
+    language: "",
+    is_favorite: false,
+  });
+
+  const handleCheckboxChange = () => {
+    setQuote({ ...quote, is_favorite: !quote.is_favorite });
+  };
+
+  const handleTextChange = (event) => {
+    setQuote({ ...quote, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!quote.quote) {
+      setErrorMessage("Quote is required.");
+      setShowModal(true);
+      return;
     }
 
-    const handleTextChange = (event) => {
-        setQoute({...quote, [event.target.id]: event.target.value });
+    if (!quote.category) {
+      setErrorMessage("Category is required.");
+      setShowModal(true);
+      return;
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addQuote(quote);
+    if (!quote.language) {
+      setErrorMessage("Language is required.");
+      setShowModal(true);
+      return;
     }
-   return (
+    addQuote(quote);
+  };
+
+  return (
     <>
-    <article className="Quote-Details">
-          <div className="card-container">
-    <div className="home-link">
-                <Link className="home-btn" to='/'><div className="home-home">Home &nbsp;&nbsp;&nbsp;| </div> </Link>&nbsp;&nbsp;
-                <Link className="home-btn" to='/quotes'><div className="home-new">Index &nbsp;&nbsp;</div></Link>
-            </div>
-            <div className="card">
-    <div className="">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="author">Author:</label>
-        <input
-          id="author"
-          value={quote.author}
-          type="text"
-          onChange={handleTextChange}
-          placeholder=" author"
-          required
-        />
-        <label htmlFor="quote">Quote:</label>
-        <input
-          id="quote"
-          type="text"
-          required
-          value={quote.quote}
-          placeholder=" quote"
-          onChange={handleTextChange}
-        />
-        <label htmlFor="category">Category:</label>
-        <input
-          id="category"
-          type="text"
-          name="category"
-          value={quote.category}
-          placeholder=" educational, inspirational..."
-          onChange={handleTextChange}
-          required
-        />
-        <label htmlFor="source">Source:</label>
-        <input
-          id="source"
-          type="text"
-          name="source"
-          value={quote.source}
-          placeholder=" source"
-          onChange={handleTextChange}
-          required
-        />
-        <label htmlFor="year_quoted">Year:</label>
-        <input
-          id="year_quoted"
-          type="text"
-          name="year_quoted"
-          value={quote.year_quoted}
-          placeholder=" date"
-          onChange={handleTextChange}
-          required
-        />
-        <label htmlFor="language">Language:</label>
-        <input
-          id="language"
-          type="text"
-          name="language"
-          value={quote.language}
-          placeholder=" language"
-          onChange={handleTextChange}
-          required
-        />
-        <label htmlFor="is_favorite">Favorite:</label>
-        <input
-          id="is_favorite"
-          type="checkbox"
-          onChange={handleCheckboxChange}
-          checked={quote.is_favorite}
-        />
+      <article className="Quote-Details">
+        <div className="card-container">
+          <div className="home-link">
+            <Link className="home-btn" to="/">
+              <div className="home-home">Home &nbsp;&nbsp;&nbsp;| </div>{" "}
+            </Link>
+            &nbsp;&nbsp;
+            <Link className="home-btn" to="/quotes">
+              <div className="home-new">Index &nbsp;&nbsp;</div>
+            </Link>
+          </div>
+          <div className="card">
+            <div className="">
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="author">Author:</label>
+                <input
+                  id="author"
+                  value={quote.author}
+                  type="text"
+                  onChange={handleTextChange}
+                  placeholder=" author"
+                />
+                <label htmlFor="quote">Quote:</label>
+                <input
+                  id="quote"
+                  type="text"
+                  value={quote.quote}
+                  placeholder=" quote"
+                  onChange={handleTextChange}
+                />
+                <label htmlFor="category">Category:</label>
+                <input
+                  id="category"
+                  type="text"
+                  name="category"
+                  value={quote.category}
+                  placeholder=" educational, inspirational..."
+                  onChange={handleTextChange}
+                />
+                <label htmlFor="source">Source:</label>
+                <input
+                  id="source"
+                  type="text"
+                  name="source"
+                  value={quote.source}
+                  placeholder=" source"
+                  onChange={handleTextChange}
+                />
+                <label htmlFor="year_quoted">Year:</label>
+                <input
+                  id="year_quoted"
+                  type="number"
+                  name="year_quoted"
+                  value={quote.year_quoted}
+                  placeholder=" date"
+                  onChange={handleTextChange}
+                />
+                <label htmlFor="language">Language:</label>
+                <input
+                  id="language"
+                  type="text"
+                  name="language"
+                  value={quote.language}
+                  placeholder=" language"
+                  onChange={handleTextChange}
+                />
+                <label htmlFor="is_favorite">Favorite:</label>
+                <input
+                  id="is_favorite"
+                  type="checkbox"
+                  onChange={handleCheckboxChange}
+                  checked={quote.is_favorite}
+                />
 
-        <br />
-        <input type="submit" />
-      </form>
-    </div>
-    </div>
-    </div>
-    </article>
+                <br />
+                <input type="submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </article>
+      <ErrorModal
+        show={showModal}
+        setShowModal={setShowModal}
+        errorMessage={errorMessage}
+      />
     </>
   );
 }
